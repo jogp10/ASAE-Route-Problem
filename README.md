d063441859e20c669ecb08d46d0b0c24c3a2f238
zearaujo11 <ze.b.araujo@gmail.com>
2023-03-13 23:24:14 +0000

    rep cleanup - forgot to delete origin folder of matplotlib

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ia_t03g34_assignment1
 Optimization Meta-Heuristic project from FEUP'S UC RCOM 
+
+matplotlib used: 
+https://github.com/Cryoris/matplotlib-cpp
--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@
 namespace plt = matplotlibcpp;
 
 int main(int argc, char* argv[]) {
-    //plt::plot({1,2,3,4},"*");
-    //plt::show();
+    plt::plot({1,2,3,4},"*");
+    plt::show();
 
     ASAE asae = ASAE();
     asae.menu();
-    
+
     return 0;
 }
 
