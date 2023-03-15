ad0d141903dde19bcb4cd514620f5e4db2f3a08c
john <jogp10@gmail.com>
2023-03-15 11:46:32 +0000

    matplot++

--- main.cpp
+++ main.cpp
@@ -2,14 +2,25 @@
 // Created by john on 20/01/22.
 //
 
-#include "src/ASAE.h"
-#include "./include/matplotlibcpp.h"
+#include <cmath>
+#include <matplot/matplot.h>
 
-namespace plt = matplotlibcpp;
+#include "src/ASAE.h"
+
+
 
 int main(int argc, char* argv[]) {
-    plt::plot({1,2,3,4},"*");
-    plt::show();
+    using namespace matplot;
+    std::vector<double> x = linspace(0, 2 * pi);
+    std::vector<double> y = transform(x, [](auto x) { return sin(x); });
+
+    plot(x, y, "-o");
+    hold(on);
+    plot(x, transform(y, [](auto y) { return -y; }), "--xr");
+    plot(x, transform(x, [](auto x) { return x / pi - 1.; }), "-:gs");
+    plot({1.0, 0.7, 0.4, 0.0, -0.4, -0.7, -1}, "k");
+
+    show();
 
     ASAE asae = ASAE();
     asae.menu();
