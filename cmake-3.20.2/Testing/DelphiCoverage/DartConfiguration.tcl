18f3d8bd88fdfc34c5f48ce52b7cb43dd5f96ff3
Zé Araújo <72151399+zearaujo11@users.noreply.github.com>
2023-03-16 09:52:36 +0000

    trying to resolve issue regarding .ignore

--- cmake-3.20.2/Testing/DelphiCoverage/DartConfiguration.tcl
+++
@@ -1,8 +0,0 @@
-# This file is configured by CMake automatically as DartConfiguration.tcl
-# If you choose not to use CMake, this file may be hand configured, by
-# filling in the required variables.
-
-
-# Configuration directories and files
-SourceDirectory: /home/zearaujo11/Programming/ia_t03g34_assignment1/cmake-3.20.2/Testing/DelphiCoverage
-BuildDirectory: /home/zearaujo11/Programming/ia_t03g34_assignment1/cmake-3.20.2/Testing/DelphiCoverage
--- cmake-3.20.2/Utilities/cmcurl/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zearaujo11/Programming/ia_t03g34_assignment1/cmake-3.20.2/Utilities/cmcurl
-# Build directory: /home/zearaujo11/Programming/ia_t03g34_assignment1/cmake-3.20.2/Utilities/cmcurl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(curl "curltest" "http://open.cdash.org/user.php")
-set_tests_properties(curl PROPERTIES  _BACKTRACE_TRIPLES "/home/zearaujo11/Programming/ia_t03g34_assignment1/cmake-3.20.2/Utilities/cmcurl/CMakeLists.txt;1468;add_test;/home/zearaujo11/Programming/ia_t03g34_assignment1/cmake-3.20.2/Utilities/cmcurl/CMakeLists.txt;0;")
-subdirs("lib")
